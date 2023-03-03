1de009b723e7d41179da958f39b78135b453b6f7
Boyu Gao <44675030+EZ4BYG@users.noreply.github.com>
2023-03-03 21:25:21 +0100

    Update 2_Python.py

--- #2/2_Python.py
+++ #2/2_Python.py
@@ -16,11 +16,11 @@ ax.set_title('Initial field: %s\n Time = %fs' % ('Spike', 0.0), fontsize = 14)  
 # Set axes tricks
 # Note: xtricks里是指定已有的索引，xtricklabels里是对应的自定义的别名
 #       就因为python是从0开始，故用xtricklabels把0重命名为1，把99重命名为100
-ax.set_xticks([0, 20, 40, 60, 80, 99])                       # Specify the x-axis tricks to these indexs
+ax.set_xticks([0, 20, 40, 60, 80, 99])                       # Set x-axis tricks to these specific indexes
 ax.set_xticklabels(['1', '20', '40', '60', '80', '100'])     # Rename the tricks 
-ax.set_yticks([0, 10, 20, 30, 40, 49])                       # set y-axis tricks
-ax.set_yticklabels(['1', '10', '20', '30', '40', '50'])      # rename tricks
+ax.set_yticks([0, 10, 20, 30, 40, 49])                       # Set y-axis tricks to these specific indexes 
+ax.set_yticklabels(['1', '10', '20', '30', '40', '50'])      # Rename the tricks
 # Final display
-plt.show()    
+plt.show()    # Only for displaying in the window. Dont use fig.show()
 # Final save
-fig.savefig('result.png', bbox_inches = 'tight', dpi = 400)  # Dont use plt.savefig
+fig.savefig('result.png', bbox_inches = 'tight', dpi = 400)  # Dont use plt.savefig()
